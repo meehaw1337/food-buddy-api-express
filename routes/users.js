905a1f2e4d78075d9b539ac25833cd1f86560fad
meehaw1337-PC <15misiek12@wp.pl>
2020-02-23 14:12:34 +0100

    Update user's products quantity endpoint added

--- routes/users.js
+++ routes/users.js
@@ -18,5 +18,31 @@ module.exports = function(sequelize) {
         .catch(error => res.status(400).send(error))
     })
 
+    router.put('/:id/products/:user_product_id', (req, res) => {
+        console.log(new Date().toLocaleString() + '  Request received: GET at user/' + req.params.id + '/products/' + req.params.user_product_id)
+
+        let updatedQuantity = req.query.updatedQuantity
+
+        /* TODO: make sure that this user_product_id belongs to the user */
+
+        if (updatedQuantity === undefined) {
+            res.status(400).send('Missing required parameter: updatedQuantity')
+        } else {
+            if (updatedQuantity > 0) {
+                UserProduct.update({quantity: updatedQuantity}, {
+                    where: {
+                        id: req.params.user_product_id
+                    }
+                }).then(() => res.status(200).send())
+            } else {
+                UserProduct.destroy({
+                    where: {
+                        id: req.params.user_product_id
+                    }
+                }).then(() => res.status(200).send())
+            }
+        }
+    })
+
     return router;
 }
